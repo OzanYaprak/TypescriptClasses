faa712add0602a75d452208e2e3d20cca429c008
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2025-08-07 09:51:33 +0300

    ##

---
+++ 12-Type Inference/app.js
@@ -0,0 +1,3 @@
+// TypeScript'te inference (çıkarım), bir değişkenin türünü açıkça belirtmeden TypeScript’in o değişkenin türünü otomatik olarak tahmin etmesidir.
+var testVariable = "ozan";
+console.log(testVariable);
---
+++ 12-Type Inference/app.ts
@@ -0,0 +1,4 @@
+// TypeScript'te inference (çıkarım), bir değişkenin türünü açıkça belirtmeden TypeScript’in o değişkenin türünü otomatik olarak tahmin etmesidir.
+
+let testVariable = "ozan";
+console.log(testVariable);
